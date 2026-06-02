ac5ababf428cf2fcd97d8196e7bc297bd8ede8d8
navd <navdeeppsharma@gmail.com>
2026-06-02 19:19:21 +0100

    agentdx 0.2.0

--- Formula/agentdx.rb
+++ Formula/agentdx.rb
@@ -1,8 +1,8 @@
 class Agentdx < Formula
   desc "Flight recorder for AI coding agents"
   homepage "https://agentdx.ai"
-  url "https://registry.npmjs.org/@agentdx/agentdx/-/agentdx-0.1.2.tgz"
-  sha256 "9f3ce09f6e63c0d1e661e1108104fb51edeafb40439f6c9dd9b6790070f2a90c"
+  url "https://registry.npmjs.org/@agentdx/agentdx/-/agentdx-0.2.0.tgz"
+  sha256 "58729c7c5b60122df3dd89c9706733c0852795d8e78b9dafa9767d2f93857ea0"
   license "MIT"
 
   depends_on "node"
@@ -13,6 +13,6 @@ class Agentdx < Formula
   end
 
   test do
-    assert_match "0.1.2", shell_output("#{bin}/agentdx --version")
+    assert_match "0.2.0", shell_output("#{bin}/agentdx --version")
   end
 end
